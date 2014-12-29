51287b805128a8d07751ce7a1d168f55aae9a0e0
Mert Akcakaya <mert@biltera.com.tr>
2014-12-29 16:32:57 +0200

    Made changes to .vimrc and added cmd.bat for windows cmd prompt

--- .vimrc
+++ .vimrc
@@ -1,2 +1,4 @@
 nmap <S-Enter> O<Esc>
 nmap <CR> o<Esc>
+set tabstop=4
+set expandtab
---
+++ cmd.bat
@@ -0,0 +1,9 @@
+@echo off
+
+:: Commands
+
+DOSKEY sd=explorer .
+
+:: Dirs
+DOSKEY gdesk=cd %USERPROFILE%\Desktop
+DOSKEY gdoc=cd %USERPROFILE%\Documents
