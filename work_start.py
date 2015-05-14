e14248ad6baa74a6853460dddf5604d4612ba9df
Mert Akcakaya <mert@biltera.com.tr>
2015-05-14 10:51:48 +0300

    Created Python script to start default programs.

---
+++ work_start.py
@@ -0,0 +1,6 @@
+import subprocess
+
+subprocess.call(['C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe', 'http:\\bloomberght.com',
+    'www.turktime.com', 'www.facebook.com', 'www.eksisozluk.com'])
+subprocess.Popen(['C:\\Program Files\\Microsoft Office\\Office15\\outlook'])
+subprocess.Popen(['C:\\Program Files (x86)\\Microsoft Visual Studio 12.0\\Common7\\IDE\\devenv.exe'])
